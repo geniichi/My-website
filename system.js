10d240a6c3a24bde0f25c76484d5db93b36242ca
geniichi <walterajcab2004@gmail.com>
2022-10-27 10:17:14 +0800

    commit..or smthn

--- system.js
+++ system.js
@@ -7,20 +7,35 @@ function num(key){
     const div = document.createElement("div");
     div.textContent = num;
     Outcontainer.appendChild(div);
+    solve(key);
 }
+
 function operator(key){
     if (key === 'clear') {
         Outcontainer.innerHTML=" ";
     } else if(key === 'plus') {
-        Outcontainer.appendChild('+');
+        let div = document.createElement("div");
+        div.textContent = "+";
+        Outcontainer.appendChild(div);
+        solve('+');
     } else if(key === 'minus') {
-        Outcontainer.appendChild('-');
+        let div = document.createElement("div");
+        div.textContent = "-";
+        Outcontainer.appendChild(div);
+    } else if(key === 'multiply') {
+        let div = document.createElement("div");
+        div.textContent = "*";
+        Outcontainer.appendChild(div);
+    } else if(key === 'divide') {
+        let div = document.createElement("div");
+        div.textContent = "/";
+        Outcontainer.appendChild(div);
+    } else if(key === 'solve') {
+        let div = document.createElement("div");
+        div.textContent = "=";
+        Outcontainer.innerText = eval(Outcontainer.innerText);
+    } else if (key === 'delete') {
+        let element = Outcontainer.lastChild;
+        element.remove();
     }
-}
-function show() {
-    question.push({
-            Fnumber: num,
-            Operator: operator,
-            Lnumber: lkey
-        });
 }
